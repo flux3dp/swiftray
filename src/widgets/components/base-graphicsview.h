7fc43b6ce28b753bb01e419f5f5c2cc3c4009667
Net Tseng <b02901184@ntu.edu.tw>
2021-09-23 14:58:27 +0800

    Reconstruct BaseGraphicsView

--- src/widgets/components/base-graphicsview.h
+++ src/widgets/components/base-graphicsview.h
@@ -8,6 +8,15 @@ public:
     BaseGraphicsView(QWidget *parent = nullptr);
     BaseGraphicsView(QGraphicsScene *scene = nullptr, QWidget *parent = nullptr);
     bool event(QEvent *e) override;
+    void reset();
+    void updateBackgroundPixmap(QPixmap background_img);
+
+private:
+    constexpr static int ITEM_ID_KEY = 0;
+    constexpr static int BACKGROUND_IMAGE_Z_INDEX = 0;
+    constexpr static char BACKGROUND_IMAGE_ITEM_ID[] = "BACKGROUND";
+    
+    QGraphicsPixmapItem* getBackgroundPixmapItem();
 
     void setMinScale(qreal min_scale) { min_scale_ = min_scale; }
 protected:
@@ -15,7 +24,7 @@ protected:
     void pinchGestureHandler(QPinchGesture *pg);
 
     qreal scaleFactor = 1;
-    qreal min_scale_ = 1;
+    qreal min_scale_ = 0.5;
     bool zooming_ = false;
     QPointF zoom_fixed_point_scene_; // the cursor location in scene when start zooming
     QPointF zoom_fixed_point_view_;  // initial zoom cursor location in view coord
