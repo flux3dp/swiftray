f9f78e1e515a9ec4b3d82dba7967cf1e2771a56e
Net Tseng <b02901184@ntu.edu.tw>
2021-10-14 10:04:04 +0800

    Support press Shift to fix the aspect ratio when drawing shaping

--- src/canvas/controls/rect.cpp
+++ src/canvas/controls/rect.cpp
@@ -10,7 +10,22 @@ bool Rect::isActive() {
 }
 
 bool Rect::mouseMoveEvent(QMouseEvent *e) {
-  rect_ = QRectF(document().mousePressedCanvasCoord(), document().getCanvasCoord(e->pos()));
+  pressed_pos_ = document().mousePressedCanvasCoord();
+  mouse_pos_ = document().getCanvasCoord(e->pos());
+  if (scale_locked_) {
+    QPointF adjusted_pos;
+    if ((mouse_pos_.y() - pressed_pos_.y()) / (mouse_pos_.x() - pressed_pos_.x()) > aspect_ratio_) {
+      adjusted_pos.setX(mouse_pos_.x());
+      adjusted_pos.setY(pressed_pos_.y() + aspect_ratio_ * (mouse_pos_.x() - pressed_pos_.x()));
+    } else {
+      adjusted_pos.setX(pressed_pos_.x() + (mouse_pos_.y() - pressed_pos_.y()) / aspect_ratio_);
+      adjusted_pos.setY(mouse_pos_.y());
+    }
+
+    rect_ = QRectF(pressed_pos_, adjusted_pos);
+  } else {
+    rect_ = QRectF(pressed_pos_, mouse_pos_);
+  }
   return true;
 }
 
@@ -36,6 +51,7 @@ void Rect::paint(QPainter *painter) {
 
 
 bool Rect::keyPressEvent(QKeyEvent *e) {
+  setScaleLock(e->modifiers() & Qt::ShiftModifier);
   if (e->key() == Qt::Key::Key_Escape) {
     exit();
     return true;
@@ -43,7 +59,17 @@ bool Rect::keyPressEvent(QKeyEvent *e) {
   return false;
 }
 
+bool Rect::keyReleaseEvent(QKeyEvent *e) {
+  setScaleLock(e->modifiers() & Qt::ShiftModifier);
+  return false;
+}
+
 void Rect::exit() {
   rect_ = QRectF(0, 0, 0, 0);
   canvas().setMode(Canvas::Mode::Selecting);
+}
+
+void Rect::setScaleLock(bool scale_lock) {
+  aspect_ratio_ = scale_lock ? (mouse_pos_.y() - pressed_pos_.y()) / (mouse_pos_.x() - pressed_pos_.x()) : 1;
+  scale_locked_ = scale_lock;
 }
