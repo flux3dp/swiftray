2be34717791036e39caac10e385d26e5c0d84987
Jason Shiao <b01901105@ntu.edu.tw>
2022-03-30 18:17:24 +0800

    Implement path preview for high speed rastering

--- src/gcode/generators/preview-generator.h
+++ src/gcode/generators/preview-generator.h
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <gcode/generators/base-generator.h>
 #include <settings/machine-settings.h>
+#include <QVector2D>
+#include <QtMath>
 
 /**
  * @brief 
@@ -39,19 +41,54 @@ class PreviewGenerator : public BaseGenerator {
     if (x_ == x && y_ == y && speed_ == speed && power_ == power)
       return;
 
-    if (x_ != x) {
-      x_ = x;
-    }
-    if (y_ != y) {
-      y_ = y;
-    }
     if (speed_ != speed) {
       speed_ = speed;
     }
     if (power_ != power) {
       power_ = power;
     }
-    paths_ << Path(QPointF(x_, y_), speed_, power_);
+
+    // Insert sub path
+    if (high_speed_raster_mode_) {
+      QVector2D vect{x - x_, y-y_};
+      vect.normalize();
+      while (true) {
+        if (high_speed_raster_idx_ >= high_speed_raster_pc_ ||
+            (high_speed_raster_idx_ / 32) > (high_speed_raster_array_.size() - 1)) {
+          if (high_speed_raster_emitting) {
+            paths_ << Path(QPointF(x_, y_), speed_, power_);
+            high_speed_raster_emitting = false;
+          }
+          break;
+        }
+        if (high_speed_raster_remaining_mm_to_next_dot_ <=
+            qSqrt(qPow(x - x_, 2) + qPow(y - y_, 2))) {
+          x_ += (vect.x() * high_speed_raster_remaining_mm_to_next_dot_);
+          y_ += (vect.y() * high_speed_raster_remaining_mm_to_next_dot_);
+          bool next_emit_state = (high_speed_raster_array_[high_speed_raster_idx_ / 32]) & ((uint32_t)(0x80000000) >> (high_speed_raster_idx_ % 32));
+          if (next_emit_state != high_speed_raster_emitting) {
+            paths_ << Path(QPointF(x_, y_), speed_, high_speed_raster_emitting ? power_ : 0);
+            high_speed_raster_emitting  = next_emit_state;
+          }
+          high_speed_raster_remaining_mm_to_next_dot_ = mm_per_dot_;
+          high_speed_raster_idx_ += 1;
+        } else {
+          high_speed_raster_remaining_mm_to_next_dot_ -= qSqrt(qPow(x - x_, 2) + qPow(y - y_, 2));
+          break;
+        }
+      }
+    }
+
+    if (x_ != x) {
+      x_ = x;
+    }
+    if (y_ != y) {
+      y_ = y;
+    }
+
+    paths_ << Path(QPointF(x_, y_), speed_,
+                   high_speed_raster_mode_ ? high_speed_raster_emitting ? power_ : 0
+                                                  : power_);
   }
 
   void setLaserPower(float power) override {
@@ -121,11 +158,93 @@ class PreviewGenerator : public BaseGenerator {
     paths_.clear();
   }
 
-  bool relative_mode_;
+  void appendCustomCmd (const std::string& cmd) override {
+    // parse D0, D1, D2, D3, D4, D5 cmd
+    QString q_str{cmd.c_str()};
+    if (q_str.startsWith("D0R") &&q_str.size() >= 4) {
+      if (cmd.at(3) == 'U') {
+        mm_per_dot_ = 0.025;
+      } else if (cmd.at(3) == 'H') {
+        mm_per_dot_ = 0.05;
+      } else if (cmd.at(3) == 'M') {
+        mm_per_dot_ = 0.1;
+      } else if (cmd.at(3) == 'L') {
+        mm_per_dot_ = 0.2;
+      }
+      high_speed_raster_mode_ = true;
+    } else if (q_str.startsWith("D1PC")) {
+      QRegularExpression re("D1PC([0-9]+)");
+      QRegularExpressionMatch match = re.match(q_str);
+      if (match.hasMatch()) {
+        high_speed_raster_pc_ = match.captured(1).toUInt();
+      }
+      high_speed_raster_array_.clear();
+      high_speed_raster_idx_ = 0;
+      high_speed_raster_remaining_mm_to_next_dot_ = 0;
+      high_speed_raster_emitting = false;
+    } else if (q_str.startsWith("D2W")) {
+      auto i = 3;
+      while (cmd.size() - i >= 8) {
+        high_speed_raster_array_.push_back(std::stoul(cmd.substr(i, 8), nullptr, 16));
+        i += 8;
+      }
+    } else if (q_str.startsWith("D3FE") || q_str.startsWith("D4PL")) {
+      // do nothing
+    } else if (q_str.startsWith("D5")) {
+      high_speed_raster_mode_ = false;
+    }
+
+  }
+  void appendCustomCmd (std::string&& cmd) override {
+    // parse D0, D1, D2, D3, D4, D5 cmd
+    QString q_str{cmd.c_str()};
+    if (q_str.startsWith("D0R") &&q_str.size() >= 4) {
+      if (cmd.at(3) == 'U') {
+        mm_per_dot_ = 0.025;
+      } else if (cmd.at(3) == 'H') {
+        mm_per_dot_ = 0.05;
+      } else if (cmd.at(3) == 'M') {
+        mm_per_dot_ = 0.1;
+      } else if (cmd.at(3) == 'L') {
+        mm_per_dot_ = 0.2;
+      }
+      high_speed_raster_mode_ = true;
+    } else if (q_str.startsWith("D1PC")) {
+      QRegularExpression re("D1PC([0-9]+)");
+      QRegularExpressionMatch match = re.match(q_str);
+      if (match.hasMatch()) {
+        high_speed_raster_pc_ = match.captured(1).toUInt();
+      }
+      high_speed_raster_array_.clear();
+      high_speed_raster_idx_ = 0;
+      high_speed_raster_remaining_mm_to_next_dot_ = 0;
+      high_speed_raster_emitting = false;
+    } else if (q_str.startsWith("D2W")) {
+      auto i = 3;
+      while (cmd.size() - i >= 8) {
+        high_speed_raster_array_.push_back(std::stoul(cmd.substr(i, 8), nullptr, 16));
+        i += 8;
+      }
+    } else if (q_str.startsWith("D3FE") || q_str.startsWith("D4PL")) {
+      // do nothing
+    } else if (q_str.startsWith("D5")) {
+      high_speed_raster_mode_ = false;
+    }
+  }
+
+
+  bool relative_mode_ = false;
+  bool high_speed_raster_mode_ = false;
+  uint32_t high_speed_raster_pc_ = 0;
+  qreal high_speed_raster_remaining_mm_to_next_dot_ = 0;
+  bool high_speed_raster_emitting = false;
+  uint32_t high_speed_raster_idx_ = 0;
+  QList<uint32_t> high_speed_raster_array_;
   int machine_width_;
   int machine_height_;
   MachineSettings::MachineSet::OriginType machine_origin_;
-  const float kHomingSpeed = 10000;
+  const qreal kHomingSpeed = 10000;
+  qreal mm_per_dot_ = 0.1;
 
   QList<Path> paths_;
 };
