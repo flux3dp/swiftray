02e3d9e61ac87f7b017e4a48110c70552836217b
GaryChunChiaChiu <106943724+GaryChunChiaChiu@users.noreply.github.com>
2022-06-23 15:04:31 +0800

    Bug/fix machine manager add new machine (#232)
    * Update machine manager add name as New Machine
    
    * Check machine number to enable remove or not
    
    * Fix minor issue
    
    Co-authored-by: Net Tseng <b02901184@ntu.edu.tw>

--- src/windows/machine-manager.cpp
+++ src/windows/machine-manager.cpp
@@ -39,6 +39,9 @@ void MachineManager::loadSettings() {
       ui->machineList->setCurrentRow(ui->machineList->count() - 1);
     }
   }
+  if(ui->machineList->count() == 1) {
+    ui->removeBtn->setEnabled(false);
+  }
 }
 
 void MachineManager::loadStyles() {
@@ -55,15 +58,20 @@ void MachineManager::registerEvents() {
   connect(ui->addBtn, &QAbstractButton::clicked, [=]() {
     QListWidgetItem *machine_item = new QListWidgetItem;
     auto machine = MachineSettings::database();
+    machine[0].name = tr("New Machine");
     machine_item->setData(Qt::UserRole, machine[0].toJson());
-    machine_item->setText("New Machine");
+    machine_item->setText(machine[0].name);
     ui->machineList->addItem(machine_item);
+    ui->removeBtn->setEnabled(true);
   });
 
   connect(ui->removeBtn, &QAbstractButton::clicked, [=]() {
     if (ui->machineList->currentItem() != nullptr) {
       auto item = ui->machineList->currentItem();
       ui->machineList->takeItem(ui->machineList->row(item));
+    }
+    if(ui->machineList->count() == 1) {
+      ui->removeBtn->setEnabled(false);
     }
   });
 
