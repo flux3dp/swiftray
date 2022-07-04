a2c2ee21faf2122da9cd945a60afee9d76ccaac3
GaryChunChiaChiu <gary@flux3dp.com>
2022-07-04 09:34:08 +0800

    add image for dark mode

--- src/windows/about-window.cpp
+++ src/windows/about-window.cpp
@@ -4,6 +4,7 @@
 #include "about-window.h"
 #include "ui_about-window.h"
 #include <QGraphicsItem>
+#include <windows/osxwindow.h>
 
 AboutWindow::AboutWindow(QWidget *parent) :
      QDialog(parent),
@@ -11,14 +12,8 @@ AboutWindow::AboutWindow(QWidget *parent) :
      BaseContainer() {
   ui->setupUi(this);
   initializeContainer();
-  // QImage image(":/images/icon.png");
-
-  // QGraphicsScene* scene = new QGraphicsScene();
-  // ui->graphicsView->setScene(scene);
-  // QGraphicsPixmapItem* item = new QGraphicsPixmapItem(QPixmap::fromImage(image));
-  // scene->addItem(item);
-
-  // ui->graphicsView->show();
+  ui->label_4->setPixmap(QPixmap(":/images/dark/icon-about.png"));
+  // ui->label_4->setPixmap(QPixmap(isDarkMode() ? ":/images/dark/icon-about.png" : ":/images/icon-about.png"));
 }
 
 AboutWindow::~AboutWindow() {
