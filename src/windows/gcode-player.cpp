19d78cdb412a01d526407eacadf4492d4c2e8f99
Jau-Shiuan(Jason) Shiao <b01901105@ntu.edu.tw>
2022-07-15 08:57:48 +0800

    Improve performance of calcRequiredTime() (#272)

--- src/windows/gcode-player.cpp
+++ src/windows/gcode-player.cpp
@@ -149,9 +149,12 @@ QString GCodePlayer::getGCode() {
 QList<QTime> GCodePlayer::calcRequiredTime() {
   QList<QTime> timestamp_list;
   QStringList gcode_list = ui->gcodeText->toPlainText().split('\n');
-  bool relative_mode = false; // G90 or G91
   int current_line = 0;
-  float last_x = 0, last_y = 0, f = 7500, x = 0, y = 0;
+  //int g_motion_modal = 0;    // 0, 1, 2, 3, 80, 81, 82, 84, 85, 86, 87, 88, 89
+  //int g_distance_modal = 90; // 90, 91
+  bool relative_mode = false; // G90 or G91
+  float last_abs_x = 0, last_abs_y = 0, last_abs_z = 0;
+  float x_param = 0, y_param = 0, z_param = 0, f_param = 7500;
 
   QTime required_time{0, 0};
 
@@ -169,60 +172,105 @@ QList<QTime> GCodePlayer::calcRequiredTime() {
     if (current_line % 100 == 0 || current_line == (gcode_list.size() - 1)) {
       progress.setValue(current_line);
     }
-    const QString& line = gcode_list[current_line];
-    if (line.startsWith("B", Qt::CaseSensitivity::CaseInsensitive)) {
+    QString line = gcode_list[current_line];
+    line = line.toUpper().section(';', 0, 0); // Eliminate comment (;)
+    if (line.startsWith("B", Qt::CaseSensitivity::CaseInsensitive) ||
+        line.startsWith("D", Qt::CaseSensitivity::CaseInsensitive) ||
+        line.startsWith("$", Qt::CaseSensitivity::CaseInsensitive)) {
       // do nothing (FLUX's custom cmd)
-    } else if (line.startsWith("D", Qt::CaseSensitivity::CaseInsensitive)) {
-      // do nothing (FLUX's custom cmd)
-    } else if (line.startsWith("$", Qt::CaseSensitivity::CaseInsensitive)) {
-      // do nothing (grbl's system cmd)
-    } else if (line.startsWith("M", Qt::CaseSensitivity::CaseInsensitive)) {
-      // do nothing (M code)
     } else {
-      if (line.indexOf("G91", 0, Qt::CaseSensitivity::CaseInsensitive) > -1) {
-        relative_mode = true;
-      } else if (line.indexOf("G90", 0, Qt::CaseSensitivity::CaseInsensitive) > -1) {
-        relative_mode = false;
-      }
+      QChar current_param{';'};
+      QString val_str;
+      line.append(' '); // To ensure the last param is processed
+      // Set default value when X/Y field is absent
+      x_param = relative_mode ? 0 : last_abs_x;
+      y_param = relative_mode ? 0 : last_abs_y;
+      z_param = relative_mode ? 0 : last_abs_z;
+      for (auto& c: line) {
+        if (c == '-' || c == '.' || c.isDigit()) {
+          if (current_param != ';') {
+            val_str.append(c);
+          }
+        } else {
+          // Finish a cmd (G-Code or M-Code)
+          if (current_param == 'G') {
+            if (val_str.toInt() == 90 || val_str.toInt() == 91) {
+              relative_mode = val_str.toInt() == 90 ? false : true;
+              // Reset default value when X/Y field is absent
+              x_param = relative_mode ? 0 : last_abs_x;
+              y_param = relative_mode ? 0 : last_abs_y;
+              z_param = relative_mode ? 0 : last_abs_z;
+            } else if (val_str.toInt() == 1) {
+              // G1 Motion modal group
+            }
+          } else if (current_param == 'M') {
+            // ignore
+          }
 
-      // default value when X/Y field is absent
-      x = relative_mode ? 0 : last_x;
-      y = relative_mode ? 0 : last_y;
+          // Finish a param
+          if (current_param == 'X') {
+            x_param = val_str.toFloat();
+          } else if (current_param == 'Y') {
+            y_param = val_str.toFloat();
+          } else if (current_param == 'Z') {
+            z_param = val_str.toFloat();
+          } else if (current_param == 'F') {
+            f_param = val_str.toFloat();
+          } else if (current_param == 'S') {
+            // ignore
+          }
 
-      if (line.indexOf("F", 0, Qt::CaseSensitivity::CaseInsensitive) > -1) {
-        QRegularExpression re("[Ff]([0-9]+([.][0-9]*)?|[.][0-9]+)");
-        QRegularExpressionMatch match = re.match(line);
-        if (match.hasMatch()) {
-          f = match.captured(1).toFloat();
+          // The start of new param
+          if (c == 'G' || c == 'M' || c == 'X' || c == 'Y' || c == 'Z' ||
+              c == 'S' || c == 'F' ) {
+            // Finish a param
+            current_param = c;
+            val_str.clear();
+          } else {
+            current_param = ';';
+            val_str.clear();
+          }
         }
       }
-      if (line.indexOf("X", 0, Qt::CaseSensitivity::CaseInsensitive) > -1) {
-        QRegularExpression re("[Xx](-?[0-9]+([.][0-9]*)?|[.][0-9]+)");
-        QRegularExpressionMatch match = re.match(line);
-        if (match.hasMatch()) {
-          x = match.captured(1).toFloat();
-        }
-      }
-      if (line.indexOf("Y", 0, Qt::CaseSensitivity::CaseInsensitive) > -1) {
-        QRegularExpression re("[Yy](-?[0-9]+([.][0-9]*)?|[.][0-9]+)");
-        QRegularExpressionMatch match = re.match(line);
-        if (match.hasMatch()) {
-          y = match.captured(1).toFloat();
-        }
-      }
 
-      Q_ASSERT_X(f > 0, "GCode Player", "Feedrate must be larger than 0");
+      /* GCode Analyze with Regex (time consuming)
+      QRegularExpression re("((?<cmd>[GM])(?<cmd_idx>[\\d]+))?(?<param>([\\t ]*[XYZSEF][\\d.-]+)+)");
+      QRegularExpressionMatch match = re.match(line);
+      if ( ! match.captured("cmd").isEmpty() && match.captured("cmd")[0] == 'G') {
+        if (match.captured("cmd_idx").toInt() >= 0 && match.captured("cmd_idx").toInt() <=3 ) {
+          // G Motion modal group
+        } else if (match.captured("cmd_idx").toInt() >= 90 && match.captured("cmd_idx").toInt() <= 91 ) {
+          // G Distance modal group
+        }
+      }
+      if ( ! match.captured("param").isEmpty()) {
+        QRegularExpression param_re("[XYZSEF][\\d.-]+");
+        QRegularExpressionMatch param_match = param_re.match(match.captured("param"));
+      }
+      */
+
+      Q_ASSERT_X(f_param > 0, "GCode Player", "Feedrate must be larger than 0");
       // NOTE: F value is in unit of mm/min
       if (relative_mode) {
-        required_time = required_time.addMSecs(1000 *
-                                               qSqrt(qPow(x, 2) + qPow(y, 2)) / f * 60);
-        last_x += x;
-        last_y += y;
+        required_time = required_time.addMSecs(
+        1000 *
+            qSqrt(qPow(x_param, 2) + qPow(y_param, 2) + qPow(z_param, 2)) /
+            f_param *
+            60);
+        last_abs_x += x_param;
+        last_abs_y += y_param;
+        last_abs_z += z_param;
       } else {
-        required_time = required_time.addMSecs(1000 *
-                                               qSqrt(qPow(x-last_x, 2) + qPow(y-last_y, 2)) / f * 60);
-        last_x = x;
-        last_y = y;
+        required_time = required_time.addMSecs(
+            1000 *
+            qSqrt(qPow(x_param-last_abs_x, 2) +
+                    qPow(y_param-last_abs_y, 2) +
+                    qPow(z_param-last_abs_z, 2)) /
+            f_param *
+            60);
+        last_abs_x = x_param;
+        last_abs_y = y_param;
+        last_abs_z = z_param;
       }
     }
 
