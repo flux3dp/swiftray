5517f1259621c39cee6d54f9c4f16dc681fe2eae
GaryChunChiaChiu <gary@flux3dp.com>
2022-07-27 09:49:34 +0800

    Add drag and drop file to canvas

---
+++ src/widgets/components/canvas-widget.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <QQuickWidget>
+
+class CanvasWidget: public QQuickWidget {
+Q_OBJECT
+
+public:
+    CanvasWidget(QWidget *parent = nullptr);
+protected:
+    void dragEnterEvent(QDragEnterEvent *event) override;
+    void dragLeaveEvent(QDragLeaveEvent *event) override;
+    void dragMoveEvent(QDragMoveEvent *event) override;
+    void dropEvent(QDropEvent *event) override;
+signals:
+    void dropFile(QPoint point, QString filename);
+};
