58dbe72df9470fb489f932968bcb5ab0694a8172
Jason Shiao <b01901105@ntu.edu.tw>
2022-10-26 15:56:50 +0800

    Fixed bug: backlash value not initialized

--- src/layer.cpp
+++ src/layer.cpp
@@ -22,6 +22,7 @@ Layer::Layer(Document *doc, const QColor &color, const QString &name) :
      speed_(20),
      power_(30),
      repeat_(1),
+     x_backlash_(0),
      parameter_index_(-1),
      is_locked_(false),
      is_visible_(true),
