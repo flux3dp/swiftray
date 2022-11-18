c17338e020c7f8ec1dd92dacc638434fb455911b
Jason Shiao <b01901105@ntu.edu.tw>
2022-11-18 12:06:15 +0800

    Remove border/shadow from the button

--- src/widgets/panels/layer-params-panel.cpp
+++ src/widgets/panels/layer-params-panel.cpp
@@ -34,6 +34,16 @@ void LayerParamsPanel::loadStyles() {
   add_layer_btn_->setGeometry(QRect(button_pos.x(), button_pos.y(), 24, 24));
   add_layer_btn_->raise();
   add_layer_btn_->show();
+  ui->parameterFrame->setStyleSheet("\
+    QToolButton#btnAddLayer:hover {\
+      background-color: transparent;\
+      border: 0px;\
+    }\
+    QToolButton#btnAddLayer{ \
+      background-color: transparent;\
+      border: 0px;\
+    } \
+  ");
 }
 
 void LayerParamsPanel::loadSettings() {
