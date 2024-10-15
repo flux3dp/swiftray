752fe6b37169d56a6a5cba531a9fb1f7d9cb5320
Simon Ko <simon@flux3dp.com>
2024-10-15 13:20:08 +0800

    Support canvas width settings

--- src/periph/motion_controller/bsl_motion_controller.cpp
+++ src/periph/motion_controller/bsl_motion_controller.cpp
@@ -205,7 +205,7 @@ void BSLMotionController::handleGcode(const QString &gcode) {
       return;
     }
 
-    QRegularExpression re("([GMXYFSZD])(-?\\d+\\.?\\d*)");
+    QRegularExpression re("([GMXYFSZDW])(-?\\d+\\.?\\d*)");
     QRegularExpressionMatchIterator i = re.globalMatch(gcode);
 
     bool is_move_command = false;
@@ -270,6 +270,11 @@ void BSLMotionController::handleGcode(const QString &gcode) {
             }
             dequeueCmd(1);
             return;
+        } else if (type == "W") {
+            double workarea = value.toDouble();
+            center_pos = workarea / 2;
+            lcs_set_scanahead_params(workarea, false, false, false, 0, 0, 0);
+            qInfo() << "BSLM~::handleGcode() - Workarea set to " << workarea << "@" << getDebugTime();
         }
     }
     if (is_handling_high_speed_ && is_move_command) {
@@ -484,25 +489,25 @@ void BSLMotionController::handleGcode(const QString &gcode) {
         if (laser_enabled && (command == "G1" || command.isEmpty())) {
             // If target_x and target_y is near x_pos_ and y_pos_, jump and mark, if too far, engrave multiple points
             if ((pow(target_x - x_pos_, 2) + pow(target_y - y_pos_, 2)) > 0.1) {
-                lcs_mark_abs(0, target_x - 55);
+                lcs_mark_abs(0, target_x - center_pos);
             } else {
-                lcs_jump_abs(0, target_x - 55);
+                lcs_jump_abs(0, target_x - center_pos);
                 lcs_laser_on_list(30);
             }
         } else {
-            lcs_jump_abs(0, target_x - 55);
+            lcs_jump_abs(0, target_x - center_pos);
         }
       } else {
         if (laser_enabled && (command == "G1" || command.isEmpty())) {
             // If target_x and target_y is near x_pos_ and y_pos_, jump and mark, if too far, engrave multiple points
             if ((pow(target_x - x_pos_, 2) + pow(target_y - y_pos_, 2)) > 0.1) {
-                lcs_mark_abs(-(target_y - 55), target_x - 55);
+                lcs_mark_abs(-(target_y - center_pos), target_x - center_pos);
             } else {
-                lcs_jump_abs(-(target_y - 55), target_x - 55);
+                lcs_jump_abs(-(target_y - center_pos), target_x - center_pos);
                 lcs_laser_on_list(100);
             }
         } else {
-            lcs_jump_abs(-(target_y - 55), target_x - 55);
+            lcs_jump_abs(-(target_y - center_pos), target_x - center_pos);
         }
       }
       x_pos_ = target_x;
