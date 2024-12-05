5a7bfa8949f0cd309b1e47b7834e9ba2894237c5
Dean Sung <dean@flux3dp.com>
2024-12-05 11:43:44 +0800

    Update version

--- src/config.h
+++ src/config.h
@@ -9,14 +9,14 @@
 #define PACKAGE "Swiftray"
 
 /* Version number of Wireshark and associated utilities */
-#define PROJECT_VERSION_STR "1.3.0"
+#define PROJECT_VERSION_STR "1.3.1"
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 3
-#define VERSION_BUILD 0
+#define VERSION_BUILD 1
 #define VERSION_BETA  0
 #define VERSION_SUFFIX ""
 #define SENTRY_DSN "https://ad81ff6a57274de58ba17f6520240929@o28957.ingest.us.sentry.io/6586888"
-#define VERSION_STRING "1.3.0"
+#define VERSION_STRING "1.3.1"
 
 /* Define to 1 if you have the Sparkle or WinSparkle library */
 /* #undef HAVE_SOFTWARE_UPDATE */
