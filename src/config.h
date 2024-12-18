d059a5d621694a73a3b9e2c9e4eaf18eaa3a8ffd
Dean Sung <dean@flux3dp.com>
2024-12-18 15:47:09 +0800

    Update version

--- src/config.h
+++ src/config.h
@@ -16,7 +16,7 @@
 #define VERSION_BETA  0
 #define VERSION_SUFFIX ""
 #define SENTRY_DSN "https://ad81ff6a57274de58ba17f6520240929@o28957.ingest.us.sentry.io/6586888"
-#define VERSION_STRING "1.3.1"
+#define VERSION_STRING "1.3.2"
 
 /* Define to 1 if you have the Sparkle or WinSparkle library */
 /* #undef HAVE_SOFTWARE_UPDATE */
