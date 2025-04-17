a8140679f9a0f8593e0fd2c3793686f468b45d4c
EstherFu <sloborfu@gmail.com>
2025-04-17 15:29:02 +0800

    Update to 1.3.9

--- src/config.h
+++ src/config.h
@@ -9,14 +9,14 @@
 #define PACKAGE "Swiftray"
 
 /* Version number of Wireshark and associated utilities */
-#define PROJECT_VERSION_STR "1.3.8"
+#define PROJECT_VERSION_STR "1.3.9"
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 3
-#define VERSION_BUILD 8
+#define VERSION_BUILD 9
 #define VERSION_BETA  0
 #define VERSION_SUFFIX ""
 #define SENTRY_DSN "https://ad81ff6a57274de58ba17f6520240929@o28957.ingest.us.sentry.io/6586888"
-#define VERSION_STRING "1.3.8"
+#define VERSION_STRING "1.3.9"
 
 /* Define to 1 if you have the Sparkle or WinSparkle library */
 /* #undef HAVE_SOFTWARE_UPDATE */
