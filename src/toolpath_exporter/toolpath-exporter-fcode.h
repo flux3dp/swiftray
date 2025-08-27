e5af4789d27df72febe8d48e062b0f1e5d903ff7
EstherFu <sloborfu@gmail.com>
2025-08-27 18:28:02 +0800

    Update rf model

--- src/toolpath_exporter/toolpath-exporter-fcode.h
+++ src/toolpath_exporter/toolpath-exporter-fcode.h
@@ -131,7 +131,7 @@ class ToolpathExporterFcode : public QObject {
   Q_OBJECT
 
  public:
-  enum class HardwareType { beamo, Beambox, BeamboxPro, HEXA, Ador, BB2, RF40, RF70 };
+  enum class HardwareType { beamo, Beambox, BeamboxPro, HEXA, Ador, BB2, RF };
 
   ToolpathExporterFcode(QTransform move_translate,
                         int dpi,
@@ -245,11 +245,8 @@ public Q_SLOTS:
       config_.z_premove_z = 0.0005;
       default_path_acc["x"] = 1000;
       default_path_acc["y"] = 1000;
-    } else if (model == "fhx2rf3" || model == "fhx2rf4") {
-      hardware_ = HardwareType::RF40;
-      is_v2_ = true;
-    } else if (model == "fhx2rf6" || model == "fhx2rf7") {
-      hardware_ = HardwareType::RF70;
+    } else if (model == "fhx2rf" || model == "fhx2rf3" || model == "fhx2rf4" || model == "fhx2rf6" || model == "fhx2rf7") {
+      hardware_ = HardwareType::RF;
       is_v2_ = true;
     } else {
       // default beambox
